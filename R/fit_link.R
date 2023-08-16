0baa27c09411d80b29dd62b6bf97a084e540f882
Daniel Casey <DCASEY@kingcounty.gov>
2023-08-16 08:49:05 -0700

    updates

--- R/fit_link.R
+++ R/fit_link.R
@@ -4,25 +4,32 @@
 #' @param ensemble a named list of parsnip model specifications
 #' @param bounds numeric. Two numbers (0-1) representing the predicted probability bounds from the screening model that should be fit with the ensemble.
 #' Results outside these bounds are considered clear enough (not-)matches to not need further processing.
-#' @importFrom stats glm
+#' @param stage_one_only logical. Whether to return the stage one results so that models can be custom implemented with the larger tidy models universe
 #' @importFrom data.table between
 #' @importFrom rsample vfold_cv
 #' @importFrom recipes recipe
 #' @importFrom workflowsets workflow_set option_add workflow_map
 #' @importFrom stacks control_stack_grid stacks add_candidates blend_predictions fit_members
+#' @importFrom stacks %>%
+#' @importFrom butcher butcher
+#' @importFrom stats glm
 #' @export
-fit_link = function(train, formula, ensemble = default_ensemble(), bounds = c(.001,.999)){
+fit_link = function(train, formula, ensemble = default_ensemble(), bounds = c(.001,.999), stage_one_only = FALSE){
 
   stopifnot(is.data.frame(train))
   stopifnot(inherits(formula, 'formula'))
   stopifnot(is.numeric(bounds))
   stopifnot(all(bounds<1 & bounds>0))
   stopifnot(bounds[1]<bounds[2])
-  modspec = sapply(ensemble, function(x) inherits(x, 'model_spec'))
-  stopifnot('Ensemble must be a list of parsnip models and inhereit model_spec' = all(modspec))
-  mtypes = sapply(ensemble, function(x) x$mode == 'classification')
-  stopifnot('Only classification type models allowed at the moment.
+  if(!stage_one_only){
+    modspec = sapply(ensemble, function(x) inherits(x, 'model_spec'))
+    stopifnot('Ensemble must be a list of parsnip models and inhereit model_spec' = all(modspec))
+    mtypes = sapply(ensemble, function(x) x$mode == 'classification')
+    stopifnot('Only classification type models allowed at the moment.
             Contact maintainer if you have a regression need' = all(mtypes))
+  }
+  stopifnot('Outcome variable must be a factor with levels 0 and 1' = is.factor(train[[as.character(theform)[[2]]]]))
+  stopifnot(all(c(0,1) %in% levels(train[[as.character(theform)[[2]]]])))
 
   # fit the models
   # start with logistic regression
@@ -31,14 +38,16 @@ fit_link = function(train, formula, ensemble = default_ensemble(), bounds = c(.0
 
   # in sample training predictions
   s1 = predict(screen, train, type = 'response')
+
+  if(stage_one_only){
+    return(list(screen = screen, bounds = bounds, i = data.table::between(s1, bounds[1], bounds[2])))
+  }
+
   trainsub = train[data.table::between(s1, bounds[1], bounds[2])]
-  fchar = as.character(formula)
-  trainsub[, (fchar[2]) := factor(get(fchar[2]))]
 
   folds = rsample::vfold_cv(trainsub, v = 5)
 
   link_rec = recipes::recipe(trainsub, formula = formula)
-
 
   wrk = workflowsets::workflow_set(preproc = list(data =link_rec),
                                    models = ensemble) %>%
@@ -49,11 +58,8 @@ fit_link = function(train, formula, ensemble = default_ensemble(), bounds = c(.0
   stk = stacks::stacks() %>% add_candidates(wrk)
 
   res = stk %>% blend_predictions() %>% fit_members()
-  res = butcher::butcher(res)
 
-  obj = list(screen = screen, stack = res, bounds = bounds)
-  class(obj) <- c('hyrule_link')
-  return(obj)
+  return(new_hyrule_link(screen, res, bounds))
 
 }
 
---
+++ R/new_hyrule_link.R
@@ -0,0 +1,18 @@
+#' Create a new hyrule_link object
+#' @param screen a glm screen model from fit_link
+#' @param stack a stacked ensemble
+#' @param bounds two item numberic vector indicatoring the bounds of where stack is relevant
+#' @export
+new_hyrule_link = function(screen, stack, bounds){
+
+  stopifnot(inherits(screen, 'glm'))
+  stopifnot(is.numeric(bounds))
+  stopifnot(all(bounds<1 & bounds>0))
+  stopifnot(bounds[1]<bounds[2])
+
+  # to do: add a stack validation
+
+  obj = list(screen = screen, stack = stack, bounds = bounds)
+  class(obj) <- c('hyrule_link')
+  obj
+}
