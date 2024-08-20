e82d7147819780222485268189a0ac664882d95f
Daniel Casey <dcasey@kingcounty.gov>
2024-08-20 08:27:23 -0700

    dev stuff

--- R/utilities.R
+++ R/utilities.R
@@ -5,26 +5,79 @@ is.Date <- function(x) inherits(x, 'Date')
 
 #' Clean a vector of names
 #' @param x character vector
-#' @param type one of 'first' or last
+#' @param bad_names a character vector of names in X to turn to NA
 #' @details
 #' Does not do splits
 #' @export
-clean_names = function(x, type = 'first'){
+clean_names = function(x, bad_names = c()){
 
-  # convert encoding
-  x = iconv(x, 'latin1', 'ASCII//TRANSLIT')
-
+  y = stringi::stri_trans_general(x, id = 'Latin-ASCII')
+  y = trimws(toupper(y))
+  # clean up punctuation and digits
   # Remove punctuation and digits
-  x = gsub(
+  y = gsub(
     "(?!-)[[:punct:]]|[[:digit:]]",
     "",
-    x,
+    y,
     perl = TRUE
   )
-  x = gsub("  ", " ", x)
-  x = gsub("[[:punct:]]|[[:space:]]", " ", x)
 
-  trimws(toupper(x))
+  # Clean up spaces
+  y = gsub("  ", " ", y)
+  y[y %in% c('1')] <- 'TRUE'
+  y = gsub("[[:punct:]]|[[:space:]]", " ", y)
 
+  # clean up JRs and SRs
+  y = gsub(' JR$', '', y)
+  y = gsub(' III$', '', y)
+  y = gsub(' II$', '', y)
+  y = gsub(' IV$', '', y)
+  y = gsub(' SR$', '', y)
+
+  # More spaces
+  y = gsub('\\s{2,}',' ',y)
+
+  # More triming
+  y = trimws(y)
+
+  y[y%in%bad_names] <- NA
+
+  y
 
 }
+
+#' Replace NA entries with the other valid entries
+#' @param x a vector
+#' @export
+fillblanks = function(x){
+  Nu = length(unique(x))
+  aNA = any(is.na(x))
+  if(aNA == TRUE & Nu == 2){
+    x[] <- unique(na.omit(x))
+  }
+
+  x
+}
+
+#' Remove spaces from a character
+#' @param x character vector
+#' @export
+remove_spaces = function(x) gsub(' ', '', x, fixed = T)
+
+#' Replace NA with blanks ('')
+#' @param x a character vector (or something to be converted to character)
+nb = function(x) {
+  x = as.character(x)
+  x[is.na(x)] <- ''
+  x
+}
+
+#' Make a row-wise hash from a data.frame (or list)
+#' @param x single level list (e.g. data.frame)
+#' @export
+make_hash = function(x){
+  x = lapply(x, nb)
+  x$sep = '|'
+  openssl::md5(do.call(paste, x))
+}
+
