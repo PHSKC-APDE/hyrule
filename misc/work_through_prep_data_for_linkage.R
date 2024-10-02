8e75ac5ffcd44e7ec671cea4fbce70ec27c86c3b
Daniel Casey <dcasey@kingcounty.gov>
2024-10-02 14:33:34 -0700

    something

--- misc/work_through_prep_data_for_linkage.R
+++ misc/work_through_prep_data_for_linkage.R
@@ -1,17 +1,23 @@
 library('hyrule')
 library('duckdb')
 library('DBI')
+library('dplyr')
 library('dbplyr')
- first_name = 'first_name'
- middle_name = 'middle_initial'
- last_name = 'last_name'
- dob = 'data_of_birth'
- ssn = NULL
- sex = 'sex'
- id = 'id1'
+first_name = 'first_name'
+middle_name = 'middle_initial'
+last_name = 'last_name'
+dob = 'date_of_birth'
+ssn = NULL
+sex = 'sex'
+id = 'id1'
 
  d = hyrule::fake_one
  ddb = DBI::dbConnect(duckdb::duckdb())
  dbWriteTable(ddb, 'df', d)
 
- d = new_hyrule_data(ddb, 'df')
+ dbt = tbl(ddb, 'df')
+
+ a = data.frame(old = 'Black', new = 'b')
+
+b =  dbt |> left_join(a, by = join_by(race_ethnicity == old), copy = T) |>
+   collect()
